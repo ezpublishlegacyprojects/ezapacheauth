c33d5fb296313672cf2ccf4f95ddb378217c072d
Gaetano Giunta <gg@ez.no>
2010-11-16 19:29:15 +0000

    - tag for release
    git-svn-id: http://svn.projects.ez.no/ezapacheauth@2 9da77c8d-7910-45cc-b2f0-f26d0cc725b0

--- trunk/extension/ezapacheauth/ezinfo.php
+++ trunk/extension/ezapacheauth/ezinfo.php
@@ -4,7 +4,7 @@ class ezapacheauthInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ezapacheauth\">ezapacheauth</a>",
-                      'Version' => "0.1-dev",
+                      'Version' => "0.1",
                       'Copyright' => "Copyright (C) 2010 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
